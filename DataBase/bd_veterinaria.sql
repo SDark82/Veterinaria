588d4c2ddb183a8a9698d6dfd072503d0dc6bc76
Samuel Cruz <74215505+SDark82@users.noreply.github.com>
2021-08-14 22:54:09 -0600

    Update bd_veterinaria.sql
    Se crearon las relaciones entre tablas y se agrego la tabla de Compra

--- DataBase/bd_veterinaria.sql
+++ DataBase/bd_veterinaria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-08-2021 a las 02:35:11
+-- Tiempo de generación: 15-08-2021 a las 06:52:35
 -- Versión del servidor: 10.4.20-MariaDB
 -- Versión de PHP: 8.0.9
 
@@ -24,21 +24,232 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `categoria_productos`
+--
+
+CREATE TABLE `categoria_productos` (
+  `Id_Categoria_Producto` int(6) NOT NULL,
+  `Nombre_Categoria` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `citas`
 --
 
 CREATE TABLE `citas` (
   `Id_Cita` int(10) NOT NULL,
   `Id_Cliente` varchar(15) NOT NULL,
-  `Id_Mascota` varchar(6) NOT NULL,
+  `Id_Mascota` int(6) NOT NULL,
   `Fecha_Cita` date NOT NULL,
   `Hora_Cita` time NOT NULL,
   `Motivo_Cita` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `ID_Cliente` varchar(15) NOT NULL,
+  `Primer_Nombre` varchar(15) NOT NULL,
+  `Segundo_Nombre` varchar(15) NOT NULL,
+  `Primer_Apellido` varchar(15) NOT NULL,
+  `Segundo_Apellido` varchar(15) NOT NULL,
+  `Telefono_Casa` int(8) NOT NULL,
+  `Telefono_Celular` int(8) NOT NULL,
+  `Correo_Electronico` varchar(50) NOT NULL,
+  `Direccion_Casa` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `compras`
+--
+
+CREATE TABLE `compras` (
+  `Id_Compra` int(6) NOT NULL,
+  `Fecha_Compra` date NOT NULL,
+  `Id_Proveedor` int(6) NOT NULL,
+  `Total_Compra` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `contactos_proveedores`
+--
+
+CREATE TABLE `contactos_proveedores` (
+  `Id_Contacto` int(6) NOT NULL,
+  `Id_Proveedor` int(6) NOT NULL,
+  `Nombre_Contacto` varchar(25) NOT NULL,
+  `Apellidos_Contacto` int(25) NOT NULL,
+  `Telefono_Celular` int(8) NOT NULL,
+  `Correo_Electronico` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle de compra`
+--
+
+CREATE TABLE `detalle de compra` (
+  `Id_Detalle_Compra` int(6) NOT NULL,
+  `Id_Compra` int(6) NOT NULL,
+  `Id_Producto` int(6) NOT NULL,
+  `Cantidad_Producto` int(6) NOT NULL,
+  `Id_Fabricante` int(11) NOT NULL,
+  `Lote` int(11) NOT NULL,
+  `Precio_Compra` float NOT NULL,
+  `IVA_Compra` float NOT NULL,
+  `Fecha_Vencimiento` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_venta`
+--
+
+CREATE TABLE `detalle_venta` (
+  `Id_Detalle_Venta` int(6) NOT NULL,
+  `Id_Venta` int(6) NOT NULL,
+  `Id_Producto` int(6) NOT NULL,
+  `Id_Servicio` int(6) NOT NULL,
+  `Cantidad` int(6) NOT NULL,
+  `SubTotal_Venta` int(11) NOT NULL,
+  `IVA_Venta` int(11) NOT NULL,
+  `Total_Venta` int(11) NOT NULL,
+  `Exento_IVA` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fabricantes`
+--
+
+CREATE TABLE `fabricantes` (
+  `Id_Fabricante` int(6) NOT NULL,
+  `Nombre` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mascotas`
+--
+
+CREATE TABLE `mascotas` (
+  `Id_Mascota` int(6) NOT NULL,
+  `ID_Cliente` varchar(15) NOT NULL,
+  `Nombre_Mascota` varchar(15) NOT NULL,
+  `Edad_Mascota` int(2) NOT NULL,
+  `Sexo` varchar(10) NOT NULL,
+  `Especie_Mascota` int(3) NOT NULL,
+  `Descripcion_Mascota` varchar(200) NOT NULL,
+  `Fecha_Registro` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `Id_Producto` int(6) NOT NULL,
+  `Codigo_Producto` varchar(20) NOT NULL,
+  `Nombre_Producto` varchar(25) NOT NULL,
+  `Presentacion_Producto` varchar(20) NOT NULL,
+  `ID_Categoria` int(6) NOT NULL,
+  `Cantidad_Producto` int(6) NOT NULL,
+  `Fecha_Vencimiento` date NOT NULL,
+  `Precio_Venta` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedores`
+--
+
+CREATE TABLE `proveedores` (
+  `Id_Proveedor` int(6) NOT NULL,
+  `RTN_Proveedor` varchar(16) NOT NULL,
+  `Nombre_Legal` varchar(200) NOT NULL,
+  `Direccion_Proveedor` varchar(200) NOT NULL,
+  `Telefono_Proveedor` int(8) NOT NULL,
+  `Estado_Proveedor` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `servicios`
+--
+
+CREATE TABLE `servicios` (
+  `Id_Servicio` int(6) NOT NULL,
+  `Nombre_Servicio` varchar(50) NOT NULL,
+  `Precio_Servicio` int(11) NOT NULL,
+  `Id_Tipo_Servicio` int(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_servicios`
+--
+
+CREATE TABLE `tipo_servicios` (
+  `Id_Tipo_Servicio` int(6) NOT NULL,
+  `Tipo_Servicio` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios_clientes`
+--
+
+CREATE TABLE `usuarios_clientes` (
+  `Id_Usuario` int(5) NOT NULL,
+  `Usuario_Cliente` varchar(15) NOT NULL,
+  `Id_Cliente` varchar(15) NOT NULL,
+  `Contraseña` varchar(25) NOT NULL,
+  `Fecha_Registro` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ventas`
+--
+
+CREATE TABLE `ventas` (
+  `Id_Venta` int(11) NOT NULL,
+  `Fecha_Venta` date NOT NULL,
+  `Id_Mascota` int(6) NOT NULL,
+  `Id_Cliente` varchar(15) NOT NULL,
+  `Total_Venta` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `categoria_productos`
+--
+ALTER TABLE `categoria_productos`
+  ADD PRIMARY KEY (`Id_Categoria_Producto`);
 
 --
 -- Indices de la tabla `citas`
@@ -49,14 +260,232 @@ ALTER TABLE `citas`
   ADD KEY `Id_Mascota` (`Id_Mascota`);
 
 --
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`ID_Cliente`),
+  ADD UNIQUE KEY `ID_Cliente` (`ID_Cliente`);
+
+--
+-- Indices de la tabla `compras`
+--
+ALTER TABLE `compras`
+  ADD PRIMARY KEY (`Id_Compra`),
+  ADD KEY `Id_Proveedor` (`Id_Proveedor`);
+
+--
+-- Indices de la tabla `contactos_proveedores`
+--
+ALTER TABLE `contactos_proveedores`
+  ADD PRIMARY KEY (`Id_Contacto`),
+  ADD KEY `Id_Proveedor` (`Id_Proveedor`);
+
+--
+-- Indices de la tabla `detalle de compra`
+--
+ALTER TABLE `detalle de compra`
+  ADD PRIMARY KEY (`Id_Detalle_Compra`),
+  ADD KEY `Id_Compra` (`Id_Compra`),
+  ADD KEY `Id_Producto` (`Id_Producto`),
+  ADD KEY `Id_Fabricante` (`Id_Fabricante`);
+
+--
+-- Indices de la tabla `detalle_venta`
+--
+ALTER TABLE `detalle_venta`
+  ADD PRIMARY KEY (`Id_Detalle_Venta`),
+  ADD KEY `Id_Venta` (`Id_Venta`),
+  ADD KEY `Id_Producto` (`Id_Producto`),
+  ADD KEY `Id_Servicio` (`Id_Servicio`);
+
+--
+-- Indices de la tabla `fabricantes`
+--
+ALTER TABLE `fabricantes`
+  ADD PRIMARY KEY (`Id_Fabricante`);
+
+--
+-- Indices de la tabla `mascotas`
+--
+ALTER TABLE `mascotas`
+  ADD PRIMARY KEY (`Id_Mascota`),
+  ADD KEY `ID_Cliente` (`ID_Cliente`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`Id_Producto`),
+  ADD KEY `ID_Categoria` (`ID_Categoria`);
+
+--
+-- Indices de la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  ADD PRIMARY KEY (`Id_Proveedor`),
+  ADD UNIQUE KEY `RTN_Proveedor` (`RTN_Proveedor`);
+
+--
+-- Indices de la tabla `servicios`
+--
+ALTER TABLE `servicios`
+  ADD PRIMARY KEY (`Id_Servicio`),
+  ADD KEY `Id_Tipo_Servicio` (`Id_Tipo_Servicio`);
+
+--
+-- Indices de la tabla `tipo_servicios`
+--
+ALTER TABLE `tipo_servicios`
+  ADD PRIMARY KEY (`Id_Tipo_Servicio`);
+
+--
+-- Indices de la tabla `usuarios_clientes`
+--
+ALTER TABLE `usuarios_clientes`
+  ADD PRIMARY KEY (`Id_Usuario`),
+  ADD KEY `Id_Cliente` (`Id_Cliente`);
+
+--
+-- Indices de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD PRIMARY KEY (`Id_Venta`),
+  ADD KEY `Id_Cliente` (`Id_Cliente`),
+  ADD KEY `Id_Mascota` (`Id_Mascota`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `categoria_productos`
+--
+ALTER TABLE `categoria_productos`
+  MODIFY `Id_Categoria_Producto` int(6) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `citas`
 --
 ALTER TABLE `citas`
   MODIFY `Id_Cita` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `compras`
+--
+ALTER TABLE `compras`
+  MODIFY `Id_Compra` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `contactos_proveedores`
+--
+ALTER TABLE `contactos_proveedores`
+  MODIFY `Id_Contacto` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle de compra`
+--
+ALTER TABLE `detalle de compra`
+  MODIFY `Id_Detalle_Compra` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_venta`
+--
+ALTER TABLE `detalle_venta`
+  MODIFY `Id_Detalle_Venta` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `fabricantes`
+--
+ALTER TABLE `fabricantes`
+  MODIFY `Id_Fabricante` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `Id_Producto` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  MODIFY `Id_Proveedor` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_servicios`
+--
+ALTER TABLE `tipo_servicios`
+  MODIFY `Id_Tipo_Servicio` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `citas`
+--
+ALTER TABLE `citas`
+  ADD CONSTRAINT `citas_ibfk_1` FOREIGN KEY (`Id_Cliente`) REFERENCES `clientes` (`ID_Cliente`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `citas_ibfk_2` FOREIGN KEY (`Id_Mascota`) REFERENCES `mascotas` (`Id_Mascota`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `compras`
+--
+ALTER TABLE `compras`
+  ADD CONSTRAINT `compras_ibfk_1` FOREIGN KEY (`Id_Proveedor`) REFERENCES `proveedores` (`Id_Proveedor`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `contactos_proveedores`
+--
+ALTER TABLE `contactos_proveedores`
+  ADD CONSTRAINT `contactos_proveedores_ibfk_1` FOREIGN KEY (`Id_Proveedor`) REFERENCES `proveedores` (`Id_Proveedor`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `detalle de compra`
+--
+ALTER TABLE `detalle de compra`
+  ADD CONSTRAINT `detalle de compra_ibfk_1` FOREIGN KEY (`Id_Fabricante`) REFERENCES `fabricantes` (`Id_Fabricante`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `detalle de compra_ibfk_2` FOREIGN KEY (`Id_Producto`) REFERENCES `productos` (`Id_Producto`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `detalle de compra_ibfk_3` FOREIGN KEY (`Id_Compra`) REFERENCES `compras` (`Id_Compra`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `detalle_venta`
+--
+ALTER TABLE `detalle_venta`
+  ADD CONSTRAINT `detalle_venta_ibfk_1` FOREIGN KEY (`Id_Servicio`) REFERENCES `servicios` (`Id_Servicio`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `detalle_venta_ibfk_2` FOREIGN KEY (`Id_Venta`) REFERENCES `ventas` (`Id_Venta`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `mascotas`
+--
+ALTER TABLE `mascotas`
+  ADD CONSTRAINT `mascotas_ibfk_1` FOREIGN KEY (`ID_Cliente`) REFERENCES `clientes` (`ID_Cliente`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `mascotas_ibfk_2` FOREIGN KEY (`Id_Mascota`) REFERENCES `ventas` (`Id_Mascota`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`Id_Producto`) REFERENCES `detalle_venta` (`Id_Producto`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `productos_ibfk_2` FOREIGN KEY (`ID_Categoria`) REFERENCES `categoria_productos` (`Id_Categoria_Producto`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `servicios`
+--
+ALTER TABLE `servicios`
+  ADD CONSTRAINT `servicios_ibfk_1` FOREIGN KEY (`Id_Tipo_Servicio`) REFERENCES `tipo_servicios` (`Id_Tipo_Servicio`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuarios_clientes`
+--
+ALTER TABLE `usuarios_clientes`
+  ADD CONSTRAINT `usuarios_clientes_ibfk_1` FOREIGN KEY (`Id_Cliente`) REFERENCES `clientes` (`ID_Cliente`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD CONSTRAINT `ventas_ibfk_1` FOREIGN KEY (`Id_Cliente`) REFERENCES `clientes` (`ID_Cliente`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
