2c821a72d2c33cee7dde60fad6d7335fb3e704a1
kathserver <80799585+kathserver@users.noreply.github.com>
2021-08-17 00:00:08 -0600

    botones

--- Proyecto/index.php
+++ Proyecto/index.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Safari || Paguina Principal</title>
-    <link rel="shortcut icon" href="static/img/Logo1.svg" type="image/x-icon">
-    <link rel="stylesheet" href="static/css/estilo.css">
+    <link rel="shortcut icon" href="statics/img/Logo1.svg" type="image/x-icon">
+    <link rel="stylesheet" href="statics/css/estilo.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800&display=swap" rel="stylesheet"> 
 </head>
 
@@ -19,7 +19,7 @@
             <a href="#">Inicio </a>
             <a href="#">Historia</a>
             <a href="#">Registrarse</a>
-            <a href="html/login.html">Ingresar</a>
+            <a href="modules/login.php">Ingresar</a>
         </nav>
         <div class="wave" style="height: 150px; overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none"
                 style="height: 100%; width: 100%;">
@@ -31,7 +31,7 @@
         <section class="contenedor sobre-nosotros">
             <h2 class="titulo">Nuestos Servicios</h2>
             <div class="contenedor-sobre-nosotros">
-                <img src="imgs/consulta.png" alt="" class="imagen-about-us">
+                <img src="statics/img/consulta.png" alt="" class="imagen-about-us">
                 <div class="contenido-textos">
                     <h3><span>1</span>Consulta</h3>
                     <p>La Clínica Veterinaria Safari cuenta con médicos veterinarios expertos en diferentes disciplinas y con la metodología necesaria para llegar a los diagnósticos que permitirán enfocar los tratamientos de manera eficiente y oportuna, seguimos a detalle el expediente clínico orientado a problemas, en el que al conjuntar los datos generales, la historia clínica y el examen clínico podemos arrojar la lista de problemas que nos permitirá agrupar los signos de acuerdo a problemas específicos y recomendar las pruebas de laboratorio e imageniología complementarias.
@@ -39,14 +39,14 @@
                   </div>
             </div>
             <div class="contenedor-sobre-nosotros">
-              <img src="imgs/cirugia.png" alt="" class="imagen-about-us">
+              <img src="statics/img/cirugia.png" alt="" class="imagen-about-us">
               <div class="contenido-textos">
                   <h3><span>2</span>Cirugía</h3>
                   <p>En la Clínica Veterinaria Safari contamos con quirófano equipado con anestesia inhalada y monitores trans quirúrgicos en donde al anestesista se le permite visualizar permanentemente las constantes de las mascotas como son el electrocardiograma, frecuencia respiratoria, cantidad de oxigeno y bióxido de carbono circulante en el organismo de nuestra mascota, entre otras  mientras son intervenidas quirúrgicamente, nuestro quirófano cuenta también con la instrumentación suficiente para la práctica de cirugía en tejidos blandos y ortopedia contando con expertos cirujanos en diferentes disciplinas.</p>
                   </div>
           </div>
           <div class="contenedor-sobre-nosotros">
-            <img src="imgs/hospital.png" alt="" class="imagen-about-us">
+            <img src="statics/img/hospital.png" alt="" class="imagen-about-us">
             <div class="contenido-textos">
                 <h3><span>3</span>Hospitalización</h3>
                 <p>La Clínica Veterinaria Safari cuenta con las instalaciones y el personal capacitado para brindar el internamiento de pacientes con fines tanto terapéuticos como diagnostico en donde se requiere la supervisión e intervención  continua (24 horas). Cuenta con la flexibilidad para ofrecer  hospitalización diurna en donde los pacientes son internados durante el día para la supervisión y administración de fármacos o tratamientos mientras los propietarios trabajan.</p>
@@ -58,23 +58,23 @@
                 <h2 class="titulo">Anuncios</h2>
                 <div class="galeria-port">
                     <div class="imagen-port">
-                        <img src="imgs/poster.png" alt="">
+                        <img src="statics/img/poster.png" alt="">
                         <div class="hover-galeria">
-                            <img src="imgs/icono1.png" alt="">
+                            <img src="statics/imgimg/icono1.png" alt="">
                             <p>ir a Anuncio</p>
                         </div>
                     </div>
                     <div class="imagen-port">
-                        <img src="imgs/poster2.png" alt="">
+                        <img src="statics/img/poster2.png" alt="">
                         <div class="hover-galeria">
-                            <img src="imgs/icono1.png" alt="">
+                            <img src="statics/imgimg/icono1.png" alt="">
                             <p>ir a Anuncio</p>
                         </div>
                     </div>
                     <div class="imagen-port">
-                        <img src="imgs/poster3.png" alt="">
+                        <img src="statics/img/poster3.png" alt="">
                         <div class="hover-galeria">
-                            <img src="imgs/icono1.png" alt="">
+                            <img src="statics/imgimg/icono1.png" alt="">
                             <p>ir a Anuncio</p>
                         </div>
                     </div>
