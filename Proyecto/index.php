865c550a2927fcea869958864b2a0767fbb39d51
kathserver <80799585+kathserver@users.noreply.github.com>
2021-08-16 23:19:51 -0600

    agregando index
    agregando un index para cliente

--- Proyecto/index.php
+++ Proyecto/index.php
@@ -1,105 +1,106 @@
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="main.css" />
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@500&display=swap" rel="stylesheet" />
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
-  <link href="https://fonts.googleapis.com/css2?family=Gowun+Dodum&display=swap" rel="stylesheet">
-  <title>Veterinaria Safari</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Safari || Paguina Principal</title>
+    <link rel="shortcut icon" href="static/img/Logo1.svg" type="image/x-icon">
+    <link rel="stylesheet" href="static/css/estilo.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800&display=swap" rel="stylesheet"> 
 </head>
 
 <body>
-  <div class="main">
-    <div class="carousel" id="carousel">
-      <div class="card1">
-        <div class="curiosidades">
-          <div class="curiosidad1" id="curiosidad1">
-            ¿Sabias que? <br />
-            <div class="blur">
+    <header>
+      <img src="static/img/Logo1.svg" alt="" style="width: 25%;padding-left: 10%;padding-top: 1%;"/>
+        <nav>
+          
+            <a href="#">Inicio </a>
+            <a href="#">Historia</a>
+            <a href="#">Registrarse</a>
+            <a href="html/login.html">Ingresar</a>
+        </nav>
+        <div class="wave" style="height: 150px; overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none"
+                style="height: 100%; width: 100%;">
+                <path d="M0.00,49.98 C150.00,150.00 349.20,-50.00 500.00,49.98 L500.00,150.00 L0.00,150.00 Z"
+                    style="stroke: none; fill: #fff;"></path>
+            </svg></div>
+    </header>
+    <main>
+        <section class="contenedor sobre-nosotros">
+            <h2 class="titulo">Nuestos Servicios</h2>
+            <div class="contenedor-sobre-nosotros">
+                <img src="imgs/consulta.png" alt="" class="imagen-about-us">
+                <div class="contenido-textos">
+                    <h3><span>1</span>Consulta</h3>
+                    <p>La Clínica Veterinaria Safari cuenta con médicos veterinarios expertos en diferentes disciplinas y con la metodología necesaria para llegar a los diagnósticos que permitirán enfocar los tratamientos de manera eficiente y oportuna, seguimos a detalle el expediente clínico orientado a problemas, en el que al conjuntar los datos generales, la historia clínica y el examen clínico podemos arrojar la lista de problemas que nos permitirá agrupar los signos de acuerdo a problemas específicos y recomendar las pruebas de laboratorio e imageniología complementarias.
+                     La parte más importante de un estetoscopio es lo que esta en medio de las olivas.</p>
+                  </div>
             </div>
-              El rugido del león puede ser escuchado<br />
-              hasta a 9 kilómetros de distancia. <br />
-              Les ayuda a encontrar a otros leones, así <br />como para
-              proclamar su territorio.
+            <div class="contenedor-sobre-nosotros">
+              <img src="imgs/cirugia.png" alt="" class="imagen-about-us">
+              <div class="contenido-textos">
+                  <h3><span>2</span>Cirugía</h3>
+                  <p>En la Clínica Veterinaria Safari contamos con quirófano equipado con anestesia inhalada y monitores trans quirúrgicos en donde al anestesista se le permite visualizar permanentemente las constantes de las mascotas como son el electrocardiograma, frecuencia respiratoria, cantidad de oxigeno y bióxido de carbono circulante en el organismo de nuestra mascota, entre otras  mientras son intervenidas quirúrgicamente, nuestro quirófano cuenta también con la instrumentación suficiente para la práctica de cirugía en tejidos blandos y ortopedia contando con expertos cirujanos en diferentes disciplinas.</p>
+                  </div>
           </div>
-          <div class="curiosidad2" id="curiosidad2">
-            ¿Sabias que? <br />
-            <div class="blur">
+          <div class="contenedor-sobre-nosotros">
+            <img src="imgs/hospital.png" alt="" class="imagen-about-us">
+            <div class="contenido-textos">
+                <h3><span>3</span>Hospitalización</h3>
+                <p>La Clínica Veterinaria Safari cuenta con las instalaciones y el personal capacitado para brindar el internamiento de pacientes con fines tanto terapéuticos como diagnostico en donde se requiere la supervisión e intervención  continua (24 horas). Cuenta con la flexibilidad para ofrecer  hospitalización diurna en donde los pacientes son internados durante el día para la supervisión y administración de fármacos o tratamientos mientras los propietarios trabajan.</p>
+              </div>
+        </div>
+        </section>
+        <section class="portafolio">
+            <div class="contenedor">
+                <h2 class="titulo">Anuncios</h2>
+                <div class="galeria-port">
+                    <div class="imagen-port">
+                        <img src="imgs/poster.png" alt="">
+                        <div class="hover-galeria">
+                            <img src="imgs/icono1.png" alt="">
+                            <p>ir a Anuncio</p>
+                        </div>
+                    </div>
+                    <div class="imagen-port">
+                        <img src="imgs/poster2.png" alt="">
+                        <div class="hover-galeria">
+                            <img src="imgs/icono1.png" alt="">
+                            <p>ir a Anuncio</p>
+                        </div>
+                    </div>
+                    <div class="imagen-port">
+                        <img src="imgs/poster3.png" alt="">
+                        <div class="hover-galeria">
+                            <img src="imgs/icono1.png" alt="">
+                            <p>ir a Anuncio</p>
+                        </div>
+                    </div>
+                  </div>
+                    </div>
+                </div>
             </div>
-              Una de las mayores curiosidades de las jirafas <br />
-              es que ellas no duermen. Los estudios demuestran <br />
-              que pueden dormir únicamente entre 10 minutos y <br />
-              2 horas. Además, duermen de pie.
-          </div>
-          <div class="curiosidad3" id="curiosidad3">
-            ¿Sabias que? <br />
-              Alrededor de 30.000 elefantes mueren al año <br />debido a cazas
-              ilegales para traficar <br />el marfil de sus colmillos.
-          </div>
-          <div class="curiosidad4" id="curiosidad4">
-            ¿Sabias que? <br />
-            <div class="blur">
+        </section>
+    </main>
+    <footer>
+        <div class="contenedor-footer">
+            <div class="content-foo">
+                <h4></h4>
+                <p></p>
             </div>
-              Los perros comprenden aproximadamente 250 palabras o gestos,
-              <br />
-              Muy similar a lo que le ocurre a un niño de 2 años.<br />
-              Por eso, es tan común que perros y niños pequeños <br />
-              se suelan llevar tan bien…
-          </div>
-          <div class="curiosidad5" id="curiosidad5">
-            ¿Sabias que? <br />
-            <div class="blur">
+            <div class="content-foo">
+                <h4></h4>
+                <p></p>
             </div>
-             Los gatos pueden pasar hasta 14 horas dormidos.<br>
-              Debido a su naturaleza nocturna, los gatos suelen <br>
-              ser mucho más hiperactivos en la tarde.
-          </div>
+            <div class="content-foo">
+                <h4></h4>
+                <p></p>
+            </div>
         </div>
-      </div>
-
-    </div>
-
-    <div class="login">
-      <div class="imglogo">
-        <img src="statics/img/Logo1Mesa de trabajo 1.svg" alt="" class="login-logo" />
-      </div>
-      
-      <div class="img-user">
-        <div class="photo" id="photo">
-          <img src="statics/img/userMesa de trabajo 1.svg" alt="" />
-          <img src="" alt="" id="photo-user" />
-        </div>
-      </div>
-      
-      <div class="text-box">
-        <div class="icon">
-          <i class="fas fa-user" style="color: white;"></i>
-        </div>
-        <input type="text" class="cuadro-texto" placeholder="Usuario" spellcheck="false" id="user" />
-      </div>
-      <div class="text-box">
-        <div class="icon">
-          <i class="fas fa-lock" style="color: white;"></i>
-        </div>
-          <input type="password" class="cuadro-texto" spellcheck="false" placeholder="Clave" id="password" />
-      </div>
-      
-      <div class="buttons">
-        <input type="submit" class="btn" value="INGRESAR" onclick="location.href = location.href = 'modules/administrador/administrador.php'" />
-      <input type="button" class="registrarse" value="REGISTRARSE" />
-      </div>
-
-  </div>
-  
-  </div>
+        <img src="statics/img/Logo1.svg" alt="" style="width: 25%;padding-left: 50%;padding-top: 1%;"/>
+    </footer>
 </body>
-<script src="main.js"></script>
 
 </html>
